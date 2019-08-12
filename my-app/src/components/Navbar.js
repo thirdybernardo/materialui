d8fe3d4429677d7305337f2ad19e4c17ec658fe3
thirdybernardo <54013921+thirdybernardo@users.noreply.github.com>
2019-08-12 21:02:31 +0800

    material

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+
+  const Navbar = () => {
+  return(
+    <div>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+        <Container maxWidth="sm">
+          <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+        </Container>
+    </div>
+  )
+}
+
+  export default Navbar;
